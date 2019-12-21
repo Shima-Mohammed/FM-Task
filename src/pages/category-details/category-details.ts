81209f008244e62108e1729001fa5606ccba4a86
shima <shymaamohammed837@gmail.com>
2019-12-21 19:59:30 +0200

    create header in category details page

--- src/pages/category-details/category-details.ts
+++ src/pages/category-details/category-details.ts
@@ -14,8 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'category-details.html',
 })
 export class CategoryDetailsPage {
+  category;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.category=this.navParams.get('category');
   }
 
   ionViewDidLoad() {
