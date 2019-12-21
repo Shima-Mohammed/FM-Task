2bc2d869a76522f759f0b1767db2de7811692240
shima <shymaamohammed837@gmail.com>
2019-12-21 20:08:37 +0200

    add menu in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { OnboardingScreenPage } from '../pages/onboarding-screen/onboarding-screen';
+import { CategoriesDataProvider } from '../providers/categories-data/categories-data';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = OnboardingScreenPage;
+  categoryData;
   
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private categoriesProvider:CategoriesDataProvider , platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.categoriesProvider.getCategoriesData().then(res=>this.categoryData = res)
   }
 }
 
